2aa349329b1a1b74575f439c897d45664a47b917
Wessl <lovewessman@gmail.com>
2021-01-11 23:53:30 +0100

    Add new level (beach), add audio effect
    Audio effects are right now limited to metallic enemies, will add for the rest. Sounds really good though!

--- slayer/Assets/EnemyMetalBall.cs
+++ slayer/Assets/EnemyMetalBall.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
@@ -14,7 +15,9 @@ public class EnemyMetalBall : MonoBehaviour
     public Sprite spriteToSwapToWhenHit;
     [Tooltip("The particle system to use when this enemy dies")]
     public GameObject deathParticleSystem;
-    
+
+    private GameObject sfx;        // reference to the prefab we will get audio info from
+
     // Private global vars
     private Light2D _myLight;
     private bool _waiting = false;
@@ -26,12 +29,14 @@ public class EnemyMetalBall : MonoBehaviour
     [Tooltip("How much this enemy will fly away from the player when hit")]
     [SerializeField] private float punchStrength = 200f;
     [SerializeField] private float waitTime = 0.1f;
+    [SerializeField] private float spinSpeed = 1000f;
     
     
     // Start is called before the first frame update
     void Start()
     {
         _waiting = true;     // set to waiting while setting things up
+        sfx = GameObject.FindWithTag("MetalBallSFX");
     }
 
     // Update is called once per frame
@@ -48,7 +53,7 @@ public class EnemyMetalBall : MonoBehaviour
         }
         // Rotate me around - depending on how visible I am ( spin is slow while spawning, maxes out when "spawn" is complete
         Color c = gameObject.GetComponentInChildren<SpriteRenderer>().color;
-        transform.Rotate(c.a * 1000f * Time.deltaTime * Vector3.forward);
+        transform.Rotate(c.a * spinSpeed * Time.deltaTime * Vector3.forward);
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -56,18 +61,25 @@ public class EnemyMetalBall : MonoBehaviour
         if (other.transform.CompareTag("SwordAttack") || other.transform.CompareTag("LongSwordAttack"))
         {
             livesLeft--;
+            var pos = transform.position;
+            var sfx1 = sfx.GetComponent<SFXControllerEnemy>();
             if (livesLeft <= 0)
             {
                 // Instantiate death effect particle system
-                Instantiate(deathParticleSystem, transform.position, Quaternion.identity);
+                Instantiate(deathParticleSystem, pos, Quaternion.identity);
+                sfx1.PlaySecondaryHitSfx();
                 Destroy(gameObject);
             }
             else
             {
                 // We have been hit, time to swap to "bloodied" sprite and shoot off in the opposite direction of the hit (assuming two lives on this guy)
-                var vectorFromPlayer = transform.position - other.transform.position;
+                var vectorFromPlayer = pos - other.transform.position;
                 bd.AddForce(vectorFromPlayer.normalized * punchStrength);
                 gameObject.GetComponentInChildren<SpriteRenderer>().sprite = spriteToSwapToWhenHit;
+                sfx1.PlayGetHitSfx();
+                // Make the movement more dramatic - enemy is now angery >:( how dare you hit me
+                spinSpeed *= 1.5f;
+                spd *= 1.5f;
             }
             
         }
